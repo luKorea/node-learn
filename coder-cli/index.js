2a2c48e68f74c67391dfbde47d700e216b45eeb6
korealu <643949593@qq.com>
2022-04-07 18:19:46 +0800

    coder-cli

--- coder-cli/index.js
+++ coder-cli/index.js
@@ -1,2 +1,11 @@
 #!/usr/bin/env node
-console.log('测试一下');
+const program = require('commander');
+
+// 1. 查看当前脚手架版本
+// 当前版本号
+const versionInfo = '当前coder-cli脚手架版本为：' + require('./package.json').version;
+// 支持小写查看版本号
+const lowerCaseCommand = '-v --version';
+program.version(versionInfo, lowerCaseCommand);
+// 2. 解析终端命令
+program.parse(process.argv);
