68dde57e1bc013703ae324122d6d8c1590334d2a
korealu <643949593@qq.com>
2022-06-22 15:58:10 +0800

    文件上传学习,完成一般 2022-06-22 15:58:01

--- uploadFile/index.js
+++ uploadFile/index.js
@@ -1,13 +1,21 @@
 const { createServer } = require('http')
- 
+const qs = require('qs')
 const server = createServer((req, res) => {
   if(req.url === '/upload' && req.method === 'POST') {
+    req.setEncoding('binary')
+    let body = ''
     req.on('data', data => {
-      console.log(data)
+      body += data
     })
     req.on('end', () => {
+      console.log(body);
+    // 1. 获取图片类型位置，截取
+      const payload = qs.parse(body, "\r\n", ": ");
+      const type = payload['Content-Type'];
+      const typeIndex = body.indexOf(type);
+      const typeLength = type.length;
       console.log('文件上传成功');
-      res.end('文件上传成功')
+      res.end('文件上传成功~~')
     })
   } else res.end('info')
 })
