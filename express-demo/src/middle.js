878bb520d878134e389333229873a3d9643369ce
korealu <643949593@qq.com>
2022-06-27 17:36:31 +0800

    exp中间件学习使用 2022-06-27 17:36:20

--- express-demo/src/middle.js
+++ express-demo/src/middle.js
@@ -1,11 +1,26 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 
 // 中间件练习，实现body中间件
-
+// app.use((req, res, next) => {
+//   if (req.headers['content-type'] === 'application/json') {
+//     req.on('data', data => {
+//       const info = JSON.parse(data.toString())
+//       req.body = info;
+//     })
+//     req.on('end', () => {
+//       next()
+//     })
+//   } else next()
+// })
+// express内置body解析中间件
+app.use(express.json())
+// express内置urlencoded
+app.use(express.urlencoded({
+  extended: true // 设置为true表示使用qs第三方库进行解析
+}))
 app.post('/login', (req, res, next) => {
-  req.on('data', (data) => {
-    console.log(data.toString());
-  })
+  console.log(req.body, 'body');
   res.end('login success')
 })
 
